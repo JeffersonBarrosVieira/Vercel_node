606c076c76a38025a2687f5461ca4b625c222dfd
jeffersonm03 <jefferson.negociom03@gmail.com>
2021-07-26 13:48:19 -0300

    users

--- api/users.js
+++ api/users.js
@@ -1,10 +1,12 @@
 module.exports = (req, res) => {
     if (req.method === 'GET') {
         res.json([
-            {name: "Jefferson", location: "Maranhão"},
-            {name: "George", location: "Pará"}
+            { name: "Jefferson", location: "Maranhão" },
+            { name: "George", location: "Pará" }
         ])
     } else {
+        const { name, location } = req.body;
 
+        res.send({ status: "User Created", name, location });
     }
 }
